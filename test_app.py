43b1f9857c8845cca6b2f2c3f16f2bf8c0b3abd3
Sethu Gopalan <sethugopalan@outlook.com>
2023-09-17 22:38:52 -0400

    jenskins update

---
+++ test_app.py
@@ -0,0 +1,21 @@
+
+from dash.testing.application_runners import import_app
+# import app
+# app_under_test = import_app(app)
+
+# Pytest test case for the callback function
+
+
+def test_update_graph():
+    # # Test the callback function
+    # runner = app_under_test.run()
+
+    # # Simulate changing the slider value to 2
+    # runner.find_element("#slider").value = 2
+    # runner.wait_for_element("#scatter-plot")
+
+    # # Verify that the scatter plot is updated correctly based on the slider value
+    # scatter_plot = runner.find_element("#scatter-plot")
+    # assert scatter_plot.get_attribute("data-x") == "[1, 2]"
+    # assert scatter_plot.get_attribute("data-y") == "[10, 11]"
+    pass
